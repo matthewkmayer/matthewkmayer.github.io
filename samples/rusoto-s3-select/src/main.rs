850c4c2471f7b89a17ab882a36804ee815364089
Matthew Mayer <matthewkmayer@gmail.com>
2018-12-06 19:13:58 -0800

    Get regular downloads working.

--- samples/rusoto-s3-select/src/main.rs
+++ samples/rusoto-s3-select/src/main.rs
@@ -2,9 +2,9 @@
 extern crate rusoto_core;
 extern crate rusoto_s3;
 
+use futures::{Future, Stream};
 use rusoto_core::Region;
 use rusoto_s3::{S3, S3Client, PutObjectRequest, GetObjectRequest, CreateBucketRequest};
-// use rusoto_core::credential::{AwsCredentials, DefaultCredentialsProvider};
 
 fn main() {
     println!("Starting up");
@@ -21,18 +21,35 @@ fn main() {
     // upload the file
     let contents: Vec<u8> = test_json.into_bytes();
     let req = PutObjectRequest {
-                bucket: test_bucket.to_owned(),
-                key: "select-sample.json".to_owned(),
-                body: Some(contents.into()),
-                ..Default::default()
-            };
+        bucket: test_bucket.to_owned(),
+        key: "select-sample.json".to_owned(),
+        body: Some(contents.into()),
+        ..Default::default()
+    };
     client.put_object(req).sync().expect("Couldn't PUT object");
 
     // run s3 select query on it, verify it only has the fields we requested
+    let get_req_select = GetObjectRequest {
+        bucket: test_bucket.to_owned(),
+        key: "select-sample.json".to_owned(),
+        ..Default::default()
+    };
+    let select_file_response = client.get_object(get_req_select).sync().expect("Couldn't download file");
+    let stream_select = select_file_response.body.unwrap();
+    let body_select = stream_select.concat2().wait().unwrap();
+    println!("body_select is '{}'", String::from_utf8(body_select).unwrap());
 
     // run regular s3 get_object, see it has all the fields
+    let get_req = GetObjectRequest {
+        bucket: test_bucket.to_owned(),
+        key: "select-sample.json".to_owned(),
+        ..Default::default()
+    };
+    let whole_file_response = client.get_object(get_req).sync().expect("Couldn't download file");
+    let stream = whole_file_response.body.unwrap();
+    let body = stream.concat2().wait().unwrap();
+    println!("body is '{}'", String::from_utf8(body).unwrap());
 
-    
     // delete the bucket
     println!("All done");
 }
